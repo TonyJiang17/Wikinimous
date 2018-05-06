835528a6ddc5caee2dee23a24c21d0ea18379bf9
hmc-squirrel-s18 <=>
2018-05-06 15:48:56 -0700

    completed instance methods in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,43 @@
 class ArticlesController < ApplicationController
-  def edit
-  end
+  before_action :article_id, only: [ :show, :edit, :update, :destroy ]
 
   def index
-  end
-
-  def new
-  end
+  	@articles = Article.all
+  end 
 
   def show
   end
+
+  def new
+  	@article = Article.new
+  end
+
+  def create 
+  	@article = Article.new(article_params)
+  	@article.save
+  	redirect_to articles_path
+  end 
+
+  def edit
+  end
+
+  def update
+  	@article.update(article_params)
+  	redirect_to article_path(@article)
+  end
+
+  def destroy
+  	@article.destroy
+  	redirect_to articles_path
+  end
+
+  private 
+
+  def article_id 
+  	@article = Article.find(params[:id])
+  end 
+
+  def article_params
+  	params.require(:article).permit(:title, :content)
+  end 
 end
