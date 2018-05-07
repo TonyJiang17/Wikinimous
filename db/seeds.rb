6ab8bcd4e6827f9dc17675a6971959c42484b82e
hmc-squirrel-s18 <=>
2018-05-06 19:15:27 -0700

    half final project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake article...'
+10.times do
+  article = Article.new(
+    title:    Faker::LeagueOfLegends.quote,
+    content: "#{Faker::LeagueOfLegends.champion} loves to use #{Faker::LeagueOfLegends.summoner_spell} and is of rank #{Faker::LeagueOfLegends.rank}"
+  )
+  article.save!
+end
+puts 'Finished!'
